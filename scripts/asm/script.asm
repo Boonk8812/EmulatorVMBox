cdbe4de652adb8ad765f139a660b72a7bd1c4538
Boonk3312 <111024718+Boonk8812@users.noreply.github.com>
2023-11-10 12:44:50 -0800

    Create script.asm

---
+++ scripts/asm/script.asm
@@ -0,0 +1,30 @@
+program:
+{
+asmProgram:
+{
+mov eax, 14
+ S1, S6: < 0000b
+ S2, S9: > 000c2
+ S7, S3: > 0d1bb
+  mov:
+   0x00000e
+
+  push eax, l4
+  pull m6, mov eax 0 -> 8
+  end:
+   *rnd
+   @random .mov eax 802, (0.7bbbc)
+   R000F
+   R55CA.R55F
+   5RRE.R551
+   0R444.9999b
+    {
+RRR8.7B.F51AE
+RRR4.5A.A8BC0
+RRR8.B5.BC551
+RRR1.BC.AA428
+RRR8.24.CC528
+RRR1.08.8888B
+    }
+}
+}
