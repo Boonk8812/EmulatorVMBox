936498fa38612d4838c9e32a082437ee2f16bcf5
Boonk3312 <theboonk7@hotmail.com>
2023-11-11 17:31:29 -0800

    Create machineimpl.asm

---
+++ scripts/asm/machineimpl.asm
@@ -0,0 +1,24 @@
+section .data
+	hello:      db 'Hello!' ; The text string plus a linefeed character
+	helloLen:   equ $-hello ; Length of the text
+	
+db "0bbbacf320828472287bcf821983 $ symbol 0ac66260bf",10
+db "0b0000001eb72968173bcf821983 $ symbol 0c5519bfad",10
+db "0b0000005829bcfa626284746213 $ symbol 0b6bc16836",10
+db "0b0000001e999ad8bcf927681983 $ symbol 0ff172bc51",10
+
+#machineImpl::asm
+ linkASM:
+ mov ebx cbbcfa86440-bbafcd61203ccfd91018
+ mov ecx be1992bcfda9e-bbfcd018108bcfdae9
+ &link();
+  global .Link
+  db "::{0bfcfd-288baf-de2134-bcffd527-0092bcfda-ffd8e72-bbcfd819-bbf1}:: = 0f298759386bbafcd259072569178165d8e018bbcd82",10
+  db "::{0bfcfd-288baf-de2134-bcffd527-0092bcfda-ffd8e72-bbcfd819-bbf1}:: = 0f298759386bbafcd9203bbafcd82901d8e018bbcd82",10
+  db "::{0bfcfd-288baf-de2134-bcffd527-bbfa7d269-661ffd9-cf829819-bbf1}:: = 0f298759386bbafcd9208bbcf0820affd8e018bbcd82",10
+  db "::{0bfcfd-288baf-de2134-bcffd527-0092bcfda-ffd8e72-bbcfd819-bbf1}:: = 0f298759386bbafcd9208bbcff272728d8e018bbcd82",10
+  db "::{0bfcfd-288baf-de2134-bcffd527-0092bcfda-ffd8e72-bbcfd819-bbf1}:: = 0f298759386bbafcd9affd0983537591d8e018bbcd82",10
+  db "::{0bfcfd-288baf-de2134-bcff2975-893763286-f873e72-b5cfd819-bbf1}:: = 0f21729961919560955076405378687396398539cd82",10
+
+  exit, endOfFile():
+  _____________________________.END(CALLE):cb0000000768185734865981376598713465867130583561427805976319183927589388488489010010294359985912533180975378958728000000000004c
